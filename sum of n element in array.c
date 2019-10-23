671086cb5a6a27a1a472947ccadc0f4d81fabcba
ABHINAV KUMAR SINGH <singhabhigkp77@gmail.com>
2019-10-24 00:07:19 +0530

    C-Algorith

---
+++ sum of n element in array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ int main()
+{
+  int n, sum = 0, c, value;
+  clrscr();
+  printf("How many numbers you want to add?\n");
+  scanf("%d", &n);
+ 
+  printf("Enter %d integers\n", n);
+ 
+  for (c = 1; c <= n; c++)
+  {
+    scanf("%d", &value);
+    sum = sum + value;
+  }
+ 
+  printf("Sum of the integers = %d\n", sum);
+ 
+  return 0;
+}
